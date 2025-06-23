71f90846794b5401cf61771e3fa6161ad925cb23
nobitamusic01 <abhaysinghchauhan2062@gmail.com>
2025-06-23 17:29:16 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/ixasta",
+                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/ll_NOBITA_DEFAULTERS_ll",
             ),
             InlineKeyboardButton(
-                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/oldskoolgc",
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/+wPjAlUcObehiZDM1",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
