ea3e86c3538392f2812d0869fd253da35f91d443
abhi9313 <108015626+abhi9313@users.noreply.github.com>
2025-07-01 13:10:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nobitamusic01/NOBITA_MUSIC01",
+    "https://github.com/abhi9313/NOBITA_MUSIC_COSTOMER",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
